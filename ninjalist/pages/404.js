668e1bed2cad8e5073ca2301c160e1597f102924
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2021-12-17 01:26:18 +0100

    8 przekierowanie z 4040 na home po 3 sekundach

--- ninjalist/pages/404.js
+++ ninjalist/pages/404.js
@@ -1,7 +1,20 @@
 // nazwa pliku WAZNA
 import Link from 'next/link';
+import { useEffect } from 'react';
+import {  useRouter } from 'next/router';
 
 const NotFound = () => {
+    const router = useRouter();
+    
+    useEffect(() => {
+        // trzy sekundy i potem przekirowanie
+        setTimeout(() => {
+            // router.go(-1) //back to history
+            // +1 forward
+            router.push('/')
+        }, 3000)
+    },[])
+    
     return ( 
         <div className="not-found">
             <h1>Ooooops...</h1>
