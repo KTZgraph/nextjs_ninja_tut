f384c69b8412230a5441a3d3aad09cc0ad5e861d
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2021-12-17 16:42:32 +0100

    10 fetching data export const getStaticProps

--- ninjalist/pages/ninjas/index.js
+++ ninjalist/pages/ninjas/index.js
@@ -1,7 +1,28 @@
-const Ninjas = () => {
+import styles from '../../styles/Ninja.module.css'; 
+
+export const getStaticProps = async () => {
+    // zostanie wykonana przed wyrenderowaniem templatu do przegladrki
+    // tylko do danych po stronie serwera - nie mozna funcku ktore sie wykonuja po stronie przegladrki
+    const rest = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await rest.json();
+
+    return {
+        props: {ninjas: data} //dane dostepne jako props w komponent template
+    }
+}
+
+const Ninjas = ({ninjas}) => {
     return ( 
         <div>
             <h1>All Ninjas</h1>
+            {ninjas.map( ninja => (
+                <div key={ninja.id}>
+                  <a className={styles.single}>
+                    <h3>{ninja.name}</h3>
+                  </a>
+                    
+                </div>
+            ))}
         </div>
      );
 }
