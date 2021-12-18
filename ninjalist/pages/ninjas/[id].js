d33feb75e205ea4d1ff53ad9441c12aaa6c09cff
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2021-12-18 21:08:44 +0100

    12 koniec getStaticPaths dla kazdego usera

--- ninjalist/pages/ninjas/[id].js
+++ ninjalist/pages/ninjas/[id].js
@@ -1,3 +1,21 @@
+export const getStaticPaths = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+
+    const paths = data.map(ninja => {
+        return {
+            // paths: [{}, {}, {params: {id} }]
+            params: {id: ninja.id.toString()}
+        }
+    })
+
+    return {
+        // paths: paths
+        paths, // nextjs produkcyjnei wygenereuje 10 stron dla kazdgeo uzytkownika z danych
+        fallback: false // fallback pages 404 gdy user chce zobaczyc uzytkownika ktorego id nie istenije
+    }
+}
+
 const Details = () => {
     return (
         <div>
